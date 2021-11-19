0ec6a11f1282829f45dfe33a3499e65d907d1be4
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2021-11-19 17:44:21 -0500

    Done with bC

---
+++ projects/partOne/pOjs/backgroundChanger.js
@@ -0,0 +1,16 @@
+const button = document.querySelector("#colorChanger");
+const body = document.querySelector("body");
+const h1 = document.querySelector("h1");
+
+button.addEventListener("click", ()=>{
+    const newColor = randomRGB();
+    body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+});
+
+const randomRGB = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+};
