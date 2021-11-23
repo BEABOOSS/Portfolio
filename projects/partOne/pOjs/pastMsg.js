995a3787689ff71bc451168e24f6757c3d9be819
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2021-11-23 12:58:24 -0500

    added main.css

--- projects/partOne/pOjs/pastMsg.js
+++ projects/partOne/pOjs/pastMsg.js
@@ -1,10 +1,26 @@
 const formInput = document.querySelector("#formInput");
 const btn = document.querySelector("#btnSubmit");
+const form = document.querySelector("#msg");
 const output = document.querySelector("#outPut");
+const elseValue = document.querySelector(".elseValue")
 
-const addMsg = () =>{
-    
+
+
+btn.addEventListener("click", addMsg());
+
+
+const addMsg = (e) =>{
+    e.preventDefaulft();
+    const userIpt = formInput.value;
+
+    if (formInput.value === ""){
+        output.classList.add("show")
+        setTimeout(fucniton(){
+            output.classList.remove("show")
+        }, 2000)
+    } else {
+        elseValue.textContent = formInput.value
+        formInput.vlaue = "";
+    }
+
 }
-
-
-btn.addEventListener("click", function(){})
