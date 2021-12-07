6308bfe55b71cba49e347d519e8939b9e879cd77
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2021-12-07 14:25:46 -0500

    Cool pong game -added computer -added ball -issue ball not moving

--- javascripSection/coolPongGame/ball.js
+++ javascripSection/coolPongGame/ball.js
@@ -1,4 +1,5 @@
-const INITIAL_VELOCITY = 0.025;
+const INITIAL_VELOCITY = 0.025
+const VELOCITY_INCREASE = 0.00001
 
 
 export default class Ball {
@@ -11,24 +12,30 @@ export default class Ball {
         return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
     }
 
-    set x() {
-        this.ballElem.style.setProperty("--y", value)
+    set x(value) {
+        this.ballElem.style.setProperty("--x", value)
     }
 
     get y() {
         return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
     }
 
-    set y() {
+    set y(value) {
         this.ballElem.style.setProperty("--y", value)
+    }
+    rect() {
+        return this.ballElem.getBoundingClientRect()
     }
 
     reset() {
         this.x = 50
         this.y = 50
         this.direction = { x: 0 }
-        while (Math.abs(this.direction) <= .2 || Math.abs(this.direction) >= 0.9) {
-            const header = randomNumberBetween(0, 2 * Math.PI)
+        while (
+            Math.abs(this.direction) <= .2 ||
+            Math.abs(this.direction) >= 0.9
+        ) {
+            const heading = randomNumberBetween(0, 2 * Math.PI)
             this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
         }
         this.velocity = INITIAL_VELOCITY
@@ -37,12 +44,21 @@ export default class Ball {
     update(delta) {
         this.x += this.direction.x * this.velocity * delta
         this.y += this.direction.y * this.velocity * delta
+        this.velocity += VELOCITY_INCREASE * delta
+        const rect = this.rect()
+
+        if (rect.bottom > - window.innerHeight || rect.top <= 0) {
+            this.direction.y *= -1
+        }
+
+        if (rect.right > - window.innerWidth || rect.left <= 0) {
+            this.direction.x *= -1
+        }
     }
 
 }
 
-
-
 function randomNumberBetweend(min, max) {
     return Math.random() * (max - min) + min
 }
+
