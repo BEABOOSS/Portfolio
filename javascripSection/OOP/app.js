3190cad75ce3acad9d1f4117f5bf1cdbf0ab942a
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2021-12-09 21:30:23 -0500

    practicing factory funcitons

---
+++ javascripSection/OOP/app.js
@@ -0,0 +1,27 @@
+function hex (r,g,b){
+    return "#" + ((1<<24) + (r << 24) + (g<<8) + b).toString(16).slice(1);
+}
+
+function rgb(r,g,b){
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+
+function makeColor(r,g,b){
+    const color =  {};
+    color.r = r;
+    color.g = g;
+    color.b = b;
+    color.rgb = function() {
+        const {r, g, b} = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+    // taken from Stack Overflow
+    color.hex = function() {
+        const {r, g, b} = this;
+        return "#" + ((1<<24) + (r << 24) + (g<<8) + b).toString(16).slice(1);
+    }
+    return color;
+}
+
+const firstColor = makeColor(35, 255, 150);
