69c466d2227ad57700bbf382442640f523841fc8
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2022-01-16 23:28:07 -0500

    did some moving

--- js/javascripSection/OOP/app.js
+++ js/javascripSection/OOP/app.js
@@ -68,6 +68,7 @@ class Color {
         this.g = g;
         this.b = b;
         this.name = name;
+        this.calcHSL();
     }
     innerRGB() {
         const { r, g, b } = this;
@@ -85,7 +86,82 @@ class Color {
     rgba(a = 1.0) {
         return `rgba(${this.innerRGB()}, ${a})`;
     }
+    hsl() {
+        const { h, s, l } = this;
+        return `hsl(${h}, ${s}%, ${l}%)`
+    }
+    opposite() {
+        const { h, s, l } = this;
+        const newHue = (h + 180) % 360;
+        return `hsl(${newHue}, ${s}%, ${l}%)`
+    }
+    calcHSL() {
+        let { r, g, b } = this;
+        r /= 255;
+        g /= 255;
+        b /= 255;
+
+        let cmin = Math.min(r, g, b),
+            cmax = Math.max(r, g, b),
+            delta = cmax - cmin,
+            h = 0,
+            s = 0,
+            l = 0;
+        if (delta == 0) h = 0;
+        else if (cmax == r) {
+            h = ((g - b) / delta) % 6;
+        } else if (cmax == g) {
+            h = (b - r) / delta + 2;
+        } else {
+            h = (r - g) / delta + 4;
+        }
+        h = Math.round(h * 60);
+
+        if (h < 0) h += 360;
+
+        l = (cmax + cmin) / 2;
+
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
+
+        this.h = h;
+        this.s = s;
+        this.l = l;
+
+    }
 }
+
 
 const c1 = new Color(255, 68, 96, "tomato");
 const c2 = new Color(11, 188, 46, 0.6, "YesHmmmHMHHmmmm");
+
+class Pet {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    eat() {
+        return `${this.name} is eating!`;
+    }
+}
+
+
+
+class Cat extends Pet{
+    constructor(name, age, livesLeft = 9){
+        super(name,age);
+        this.livesLeft = livesLeft;
+    }
+    meow() {
+        return "MEOOWWWWWW!!!!!";
+    }
+}
+
+
+class Dog extends Pet{
+    bark() {
+        return "WOOOFFFFF!!!!!!!";
+    }
+}
