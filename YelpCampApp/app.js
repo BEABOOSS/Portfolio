c120d29e0dd576ecb67536a81a19c8ca53c6048c
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2022-02-03 12:46:16 -0500

    basic error handle

--- YelpCampApp/app.js
+++ YelpCampApp/app.js
@@ -1,9 +1,11 @@
+const expressError = require("./utils/expressError")
+const methodOverride = require("method-override");
+const Campground = require("./models/campground");
+const catchAsync = require("./utils/catchAsync")
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const express = require("express");
 const path = require("path");
-const mongoose = require("mongoose");
-const Campground = require("./models/campground");
-const methodOverride = require("method-override");
-const ejsMate = require("ejs-mate");
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -18,7 +20,7 @@ db.once("open", () => {
 const app = express();
 
 app.engine("ejs", ejsMate);
-app.set("views", path.join(__dirname, "/views")); 
+app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 app.use(methodOverride("_method"))
@@ -29,40 +31,48 @@ app.use(express.urlencoded({ extended: true }));
 
 //* ROUTES //* CRUD --->> order matter when putting your routes
 
-app.get("/campgrounds", async (req, res) => {
+app.get("/campgrounds", catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render("campgrounds/index", { campgrounds })
-});
+}))
+
 // making new campgrounds
 app.get("/campgrounds/new", (req, res) => {
     res.render("campgrounds/new");
-});
-app.post("/campgrounds", async (req, res) => {
+})
+app.post("/campgrounds", catchAsync( async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-})
+}))
 
 //* Showing campground by the id
-app.get("/campgrounds/:id", async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+app.get("/campgrounds/:id", catchAsync( async (req, res) => {
+    const campground = catchAsync( await Campground.findById(req.params.id));
     res.render("campgrounds/show", { campground });
-});
+}))
+
 //* Updating
-app.get("/campgrounds/:id/edit", async (req, res) => {
+app.get("/campgrounds/:id/edit", catchAsync( async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/edit", { campground });
-})
-app.put("/campgrounds/:id", async(req, res) => {
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+}))
+app.put("/campgrounds/:id", catchAsync( async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 // delete will need to be changed to have the rights to do so
-app.delete("/campgrounds/:id", async(req, res) => {
-    const {id} = req.params;
+app.delete("/campgrounds/:id", catchAsync( async (req, res) => {
+    const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
+}))
+
+
+// error middleware
+app.use((err, req, res, next) => {
+    res.send("Ohh boii something went wrong");
 })
 
 app.listen(3000, () => {
