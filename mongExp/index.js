6f7f40132caee20b15be2195f31d2cb1b4e10dec
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2022-02-07 14:05:05 -0500

    connecting products to farms with IDs

--- mongExp/index.js
+++ mongExp/index.js
@@ -1,10 +1,11 @@
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
-const mongoose = require("mongoose");
-const methodOverride = require("method-override");
 const app = express();
 
 const Product = require("./models/product");
+const Farm = require("./models/farm")
 
 mongoose.connect('mongodb://localhost:27017/farmStand')
     .then(() => {
@@ -28,6 +29,49 @@ app.use(methodOverride("_method"))
 //*==========================================================
 
 
+// FARM ROUTES
+app.get("/farms", async (req, res) => {
+    const farms = await Farm.find({});
+    res.render("farms/index", { farms });
+})
+
+app.get("/farms/new", (req, res) => {
+    res.render('farms/new')
+})
+
+app.get("/farms/:id", async (req, res) => {
+    const farm = await Farm.findById(req.params.id).populate("products");
+    res.render("farms/show", { farm });
+})
+app.post("/farms", async (req, res) => {
+    const farm = new Farm(req.body);
+    await farm.save();
+    res.redirect("/farms")
+})
+
+
+
+
+
+// PRODUCTS ROUTS
+app.get("/farms/:id/products/new", async(req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    res.render("products/new", { categories, farm })
+})
+
+
+app.post("/farms/:id/products", async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, categories } = req.body;
+    const product = new Product({ name, price, categories });
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.redirect(`/farms/${farm._id}`)
+})
 
 
 
@@ -43,7 +87,7 @@ app.get("/products", async (req, res) => {
         res.render("products/index", { products, category })
     } else {
         const products = await Product.find({})
-        res.render("products/index", { products, category: "All"})   
+        res.render("products/index", { products, category: "All" })
     }
 })
 
@@ -69,8 +113,8 @@ app.post("/products", async (req, res) => {
 
 app.get("/products/:id", async (req, res) => {
     const { id } = req.params;
-    const foundProduct = await Product.findById(id)
-    res.render("products/show", { foundProduct })
+    const foundProduct = await Product.findById(id).populate("farm");
+    res.render("products/show", { foundProduct });
 })
 
 
