8a6616eb40cdce8605a6e4bb96fbe9c33f725a82
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2022-02-08 16:25:31 -0500

    Cookie parser

--- cookies/index.js
+++ cookies/index.js
@@ -1,14 +1,30 @@
 const shleterRoutes = require("./routes/shelters");
 const adminRoutes = require("./routes/admin");
+const cookieParser = require("cookie-parser")
 const dogsRoutes = require("./routes/dogs");
 const express = require("express");
 const app = express();
 
 
-
+app.use(cookieParser());
 app.use("/shelters", shleterRoutes);
 app.use("/dogs", dogsRoutes);
 app.use("/admin", adminRoutes);
+
+
+
+
+
+app.get("/greet", (req, res) => {
+    const {name = "noname"} = req.cookies;
+    res.send(`Hey There,${name}`)
+})
+app.get("/setname", (req, res) => {
+    res.cookie("name", "stevie chicks")
+    res.send("OK SENT YOU A COOKIE!!!!!!")
+})
+
+
 
 
 app.listen(3000, () => {
