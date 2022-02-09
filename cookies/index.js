8da10b054573c5222aed42e6a07db981d201c2d2
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2022-02-09 13:55:34 -0500

    moved some folders

--- cookies/index.js
+++ cookies/index.js
@@ -1,12 +1,15 @@
 const shleterRoutes = require("./routes/shelters");
 const adminRoutes = require("./routes/admin");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 const dogsRoutes = require("./routes/dogs");
+const session = require("express-session");
 const express = require("express");
 const app = express();
 
 
-app.use(cookieParser("thisismysecret"));
+app.use(session({ secret: "thisisnotagoodsecret", resave: false, saveUninitialized: false }));
+
+// app.use(cookieParser("thisismysecret"));
 app.use("/shelters", shleterRoutes);
 app.use("/dogs", dogsRoutes);
 app.use("/admin", adminRoutes);
@@ -15,10 +18,10 @@ app.use("/admin", adminRoutes);
 
 
 
-app.get("/greet", (req, res) => {
-    const { name = "noname" } = req.cookies;
-    res.send(`Hey There, ${name}`);
-})
+// app.get("/greet", (req, res) => {
+//     const { name = "noname" } = req.cookies;
+//     res.send(`Hey There, ${name}`);
+// })
 app.get("/setname", (req, res) => {
     res.cookie("name", "stevie chicks");
     res.send("OK SENT YOU A COOKIE!!!!!!");
@@ -27,8 +30,33 @@ app.get("/getsignedcookie", (req, res) => {
     res.cookie("fruit", "grape", { signed: true });
     res.send("OK SIGNED YOUR COOKIE");
 })
-app.get("/verifyfruit", (req, res) => {
-    res.send(req.signedCookies);
+// app.get("/verifyfruit", (req, res) => {
+//     res.send(req.signedCookies);
+// })
+
+
+app.get("/viewcount", (req, res) => {
+    if (req.session.count) {
+        req.session.count += 1;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`You have viewed this page ${req.session.count} times`)
+})
+
+
+
+
+
+app.get("/register", (req, res) => {
+    const { username = "Anonymous" } = req.query;
+    req.session.username = username;
+    res.redirect("/greet");
+})
+
+app.get("/greet", (req, res) => {
+    const {username} = req.session;
+    res.send(`Hey there, ${username}`)
 })
 
 
