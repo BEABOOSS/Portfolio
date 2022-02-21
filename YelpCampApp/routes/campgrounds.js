7cde851e5256552373c5c9fc10e30145fc7e4d62
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2022-02-21 13:13:20 -0500

    added flash for ~errors

--- YelpCampApp/routes/campgrounds.js
+++ YelpCampApp/routes/campgrounds.js
@@ -40,12 +40,20 @@ router.post("/", validateCampground, catchAsync(async (req, res, next) => {
 //* Showing campground by the id
 router.get("/:id", catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate("reviews");
+    if(!campground){
+        req.flash("error", "Campground not found!");
+        return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/show", { campground });
 }))
 
 //* Updating
 router.get("/:id/edit", catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if(!campground){
+        req.flash("error", "I Don't Know! I Didn't Find That One. Recheck If You Entered The Right Thing.");
+        return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/edit", { campground });
 }));
 router.put("/:id", validateCampground, catchAsync(async (req, res) => {
