f5212141ba1be656f674e32dfdd67d42de150667
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2022-02-22 16:00:05 -0500

    added login && sign up

--- auth/app.js
+++ auth/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const app = express();
 const User = require("./models/user");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
 
 mongoose.connect("mongodb://localhost:27017/authDemo")
     .then(() => {
@@ -10,15 +12,48 @@ mongoose.connect("mongodb://localhost:27017/authDemo")
     .catch(err => {
         console.log("OHH NO MONGO CONNECTION ERROR!!!!");
         console.log(err);
-    })
+    });
 
 
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "notagoodsecret" }))
+
+app.get("/", (req, res) => {
+    res.send("THIS THE HOME PAGE!");
+})
+
 app.get("/register", (req, res) => {
     res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({ username, password: hash });
+    await user.save();
+    res.redirect("/");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+
+app.post("/login", async (req, res) => {
+    const { password, username } = req.body;
+    const user = await User.findOne({ username });
+    const validePassword = await bcrypt.compare(password, user.password);
+    if (validePassword) {
+        
+        res.send("YAY WELCOME");
+    } else {
+        res.send("TRY AGAIN!");
+
+    }
 })
 
 
