dd677e24e3847960bff0ffe9caecd6e6dc65c6bb
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2022-02-22 20:38:27 -0500

    basic auth

--- auth/app.js
+++ auth/app.js
@@ -19,6 +19,13 @@ mongoose.connect("mongodb://localhost:27017/authDemo")
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect("/login")
+    }
+    next();
+}
+
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: "notagoodsecret" }))
 
@@ -32,8 +39,7 @@ app.get("/register", (req, res) => {
 
 app.post("/register", async (req, res) => {
     const { password, username } = req.body;
-    const hash = await bcrypt.hash(password, 12);
-    const user = new User({ username, password: hash });
+    const user = new User({ username, password });
     await user.save();
     req.session.user_id = user._id;
     res.redirect("/");
@@ -46,10 +52,9 @@ app.get("/login", (req, res) => {
 
 app.post("/login", async (req, res) => {
     const { password, username } = req.body;
-    const user = await User.findOne({ username });
-    const validePassword = await bcrypt.compare(password, user.password);
-    if (validePassword) {
-        req.session.user_id = user._id;
+    const foundUser = await User.findAndValidate(username, password);
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
         res.redirect("/secret");
     } else {
         res.redirect("/login");
@@ -62,12 +67,13 @@ app.post("/logout", (req, res) => {
     res.redirect("/login");
 })
 
-app.get("/secret", (req, res) => {
-    if(!req.session.user_id){
-        return res.redirect("/login");
-    }
+app.get("/secret", requireLogin, (req, res) => {
     res.render("secret");
 });
+
+app.get("/topsecret", requireLogin, (req, res) => {
+    res.send("TOP SECRET!!!")
+})
 
 
 
