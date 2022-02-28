c4e152cfbb5750f78942600a8f75628e7f683582
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2022-02-28 13:45:48 -0500

    - added login - doesn't retain if there actually login

--- YelpCampApp/app.js
+++ YelpCampApp/app.js
@@ -6,10 +6,18 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+
+
+
+
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
+const userRoutes = require("./routes/users");
 
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
@@ -46,6 +54,14 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser ());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
@@ -53,12 +69,18 @@ app.use((req, res, next) => {
 })
 
 
+
+app.get("/fakeUser", async (req, res)  => {
+    const user = new User({email: "warrenlestum@gmail.com", username: "BEEBS"})
+    const newUser = await User.register(user, "monkey");
+    res.send(newUser); 
+})
+
+
 // IMPORTING ROUTES 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
-
-
-
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 
 
