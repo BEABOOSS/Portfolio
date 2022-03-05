8d35b67e2f1e32e2b508f7bc55c565a29e14a743
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2022-03-05 17:18:20 -0500

    - added controllers - added stars review will need to come back to it though

--- YelpCampApp/routes/reviews.js
+++ YelpCampApp/routes/reviews.js
@@ -1,33 +1,14 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const Campground = require("../models/campground");
-const Review = require("../models/review");
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 const catchAsync = require("../utils/catchAsync");
-
-
-
+const reviews = require("../controllers/review");
 
 // Making a review
-router.post("/", isLoggedIn, validateReview, catchAsync(async (req, res, next) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash("success", "Created New Review")
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
+router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview))
 
 // deleting reviews /pulling any matching ID of that array and deleting it 
-router.delete("/:reviewId", isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Successfully deleted Review")
-    res.redirect(`/campgrounds/${id}`);
-}))
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 
 module.exports = router;
