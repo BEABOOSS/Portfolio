3f69c76bc01196385acbb80192ee01029abdbfde
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2022-03-19 19:24:23 -0400

    security stuff

--- YelpCampApp/app.js
+++ YelpCampApp/app.js
@@ -1,7 +1,6 @@
-if(process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
 }
-
 
 const express = require("express");
 const path = require("path");
@@ -15,21 +14,20 @@ const methodOverride = require("method-override");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
-
+const helmet = require("helmet");
 
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 const userRoutes = require("./routes/users");
 
-
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
-// checks if there is an error 
+// checks if there is an error
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-// if database opened prints :  
+// if database opened prints :
 db.once("open", () => {
-    console.log("Database connected");
+	console.log("Database connected");
 });
 
 const app = express();
@@ -39,24 +37,74 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"))
-app.use(express.static(path.join(__dirname, "public")))
-app.use(mongoSanitize())
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
 
 const sessionConfig = {
-    secret: "thisshouldbeabettersecret!",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-
-    }
-}
+	name: "rooster",
+	secret: "thisshouldbeabettersecret!",
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		// V  ENABLE THIS  BEFORE PUTTING IT IN PRODUCTION V
+		// secure: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+	},
+};
 app.use(session(sessionConfig));
 app.use(flash());
+// app.use(helmet());
 
+const scriptSrcUrls = [
+	"https://stackpath.bootstrapcdn.com/",
+	"https://api.tiles.mapbox.com/",
+	"https://api.mapbox.com/",
+	"https://kit.fontawesome.com/",
+	"https://cdnjs.cloudflare.com/",
+	"https://cdn.jsdelivr.net/",
+	"https://res.cloudinary.com/dqdaf6ffk/",
+];
+const styleSrcUrls = [
+	"https://kit-free.fontawesome.com/",
+	"https://stackpath.bootstrapcdn.com/",
+	"https://api.mapbox.com/",
+	"https://api.tiles.mapbox.com/",
+	"https://fonts.googleapis.com/",
+	"https://use.fontawesome.com/",
+	"https://cdn.jsdelivr.net/",
+	"https://res.cloudinary.com/dqdaf6ffk/",
+];
+const connectSrcUrls = ["https://*.tiles.mapbox.com", "https://api.mapbox.com", "https://events.mapbox.com", "https://res.cloudinary.com/dqdaf6ffk/"];
+const fontSrcUrls = ["https://res.cloudinary.com/dqdaf6ffk/"];
+
+app.use(
+	helmet({
+		contentSecurityPolicy: {
+			directives: {
+				defaultSrc: [],
+				connectSrc: ["'self'", ...connectSrcUrls],
+				scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+				styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+				workerSrc: ["'self'", "blob:"],
+				objectSrc: [],
+				imgSrc: [
+					"'self'",
+					"blob:",
+					"data:",
+					"https://res.cloudinary.com/dqdaf6ffk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+					"https://images.unsplash.com/",
+				],
+				fontSrc: ["'self'", ...fontSrcUrls],
+				mediaSrc: ["https://res.cloudinary.com/dlzez5yga/"],
+				childSrc: ["blob:"],
+			},
+		},
+		crossOriginEmbedderPolicy: false,
+	})
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -66,42 +114,36 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    console.log(req.query);
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    next();
-})
+	console.log(req.query);
+	res.locals.currentUser = req.user;
+	res.locals.success = req.flash("success");
+	res.locals.error = req.flash("error");
+	next();
+});
 
-// IMPORTING ROUTES 
+// IMPORTING ROUTES
 app.use("/", userRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
 
-
-
 //* ROUTES //* CRUD --->> order matter when putting your routes
 app.get("/", (req, res) => {
-    res.render("home")
+	res.render("home");
 });
-
-
-
-
 
 //* ==================
 //*  error middleware
 //* ==================
 app.all("*", (req, res, next) => {
-    next(new ExpressError("Page Not Found", 404))
+	next(new ExpressError("Page Not Found", 404));
 });
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    if (!err.message) err.message = "Ohh No, Something Went Wrong!!!";
-    res.status(statusCode).render("error", { err });
+	const { statusCode = 500 } = err;
+	if (!err.message) err.message = "Ohh No, Something Went Wrong!!!";
+	res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
-    console.log("Serving on port 3000");
-}); 
+	console.log("Serving on port 3000");
+});
