86f4f69cfd1653dfc6ae7f4038cb1084718464c6
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2022-03-19 17:31:35 -0400

    - more styles (register, +/- on map)

--- YelpCampApp/app.js
+++ YelpCampApp/app.js
@@ -9,6 +9,7 @@ const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("connect-flash");
+const mongoSanitize = require("express-mongo-sanitize");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const passport = require("passport");
@@ -40,6 +41,7 @@ app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"))
 app.use(express.static(path.join(__dirname, "public")))
+app.use(mongoSanitize())
 
 const sessionConfig = {
     secret: "thisshouldbeabettersecret!",
@@ -64,7 +66,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    console.log(req.session);
+    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
